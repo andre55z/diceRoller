d7a901198c580916dfd4c4ca4359ae8cb5d67a35
andredev <andluccag@gmail.com>
2025-03-16 10:58:22 -0300

    Commit final

--- diceRoller/Program.cs
+++ diceRoller/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Random;
+
+namespace DiceRoller
+{
+    class Dice
+    {
+        string diceColor = "Black";
+        int diceSides;
+        public void Talking(){
+            Console.WriteLine("Hello! Welcome to the Dice Roller!\nHow many sides does your dice have?");
+            diceSides = Convert.ToInt32(Console.ReadLine());
+            Rolling();
+
+        }
+        public void Rolling()
+        {
+            Console.WriteLine(@"OK, lets roll? (Press ENTER)");
+            Console.ReadLine();
+            Random random= new Random();
+            diceSides = random.Next(1,diceSides);
+            Console.WriteLine($@"The number is {diceSides}");
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dice dice = new Dice();
+            dice.Talking();
+        }  
+    }
+}
